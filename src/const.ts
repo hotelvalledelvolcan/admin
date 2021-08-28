1fb87523df9d5309aa26608d1251b11274a3c6ae
Laucha Carro <lautaroecarro@gmail.com>
2021-08-28 01:39:49 -0300

    pongo la Url de produccion

--- src/const.ts
+++ src/const.ts
@@ -1,1 +1,4 @@
-export const baseUrl = process.env.NODE_ENV === 'development' ? 'https://localhost:44316/api/v1/hotelvalledelvolcan-module' : 'https://car.marnology.com'
+export const baseUrl =
+  process.env.NODE_ENV === "development"
+    ? "https://localhost:44316/api/v1/hotelvalledelvolcan-module"
+    : "https://aivenweb-backend.herokuapp.com/api/v1/hotelvalledelvolcan-module";
